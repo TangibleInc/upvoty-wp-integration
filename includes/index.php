df6cec5116f8763ce7e4fbff0e1936acfcf65585
eliot-akira <me@eliotakira.com>
2020-01-17 19:11:35 +0100

    Prepare for third-party integrations: Elementor, Beaver Builder, Gutenberg

--- includes/index.php
+++ includes/index.php
@@ -5,3 +5,6 @@ require __DIR__ . '/settings/index.php';
 require __DIR__ . '/shortcode.php';
 require __DIR__ . '/user.php';
 require __DIR__ . '/widget.php';
+
+// Integrations with page builders
+require __DIR__ . '/integrations/index.php';
---
+++ includes/integrations/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Third-party integrations
+ *
+ * Load conditionally when dependencies are met
+ *
+ * @local $framework, $upvoty
+ */
+
+if ( did_action( 'elementor/loaded' ) ) {
+  require __DIR__ . '/elementor/index.php';
+}
+
+if ( class_exists('FLBuilder') ) {
+  require __DIR__ . '/beaver/index.php';
+}
+
+if ( function_exists('has_blocks') ) {
+  require __DIR__ . '/gutenberg/index.php';
+}
