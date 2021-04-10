43bddb3491619fd5bd5ee02ad0a0a3165d2ac89e
slobo <brbaso@gmail.com>
2021-04-10 10:34:58 +0200

    test

--- includes/index.php
+++ includes/index.php
@@ -7,5 +7,5 @@ require __DIR__ . '/shortcode.php';
 require __DIR__ . '/user.php';
 require __DIR__ . '/widget/index.php';
 
-// Integrations with page builders test..
+// Integrations with page builders
 require __DIR__ . '/integrations/index.php';
